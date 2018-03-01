3f03a970a8d25b3d26039143ce02450227a4ac65
Anne Faber <anne.faber@txstate.edu>
2018-03-01 09:57:07 -0600

    Split service activities into 2 columns. Display them as one column on small screens

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,15 @@
 module.exports = {
   yell : function(s, options) {
     return s.toUpperCase();
+  },
+  eachHalf : function(list, index, options) {
+    var arrayChunk= "";
+    var splitPoint = Math.ceil(list.length / 2);
+    var start = (index == 0) ? 0 : splitPoint;
+    var end = (index == 0) ? splitPoint : list.length;
+    list.slice(start,end).forEach(function(item) {
+      arrayChunk += options.fn(item)
+    });
+    return arrayChunk;
   }
 }
