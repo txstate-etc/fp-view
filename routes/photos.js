16321e022fb22e192fa0cdf6e08217383ad88cfb
wickning1 <git@mythgaming.net>
2018-05-08 18:50:13 -0500

    fix pagination on photos endpoint

--- routes/photos.js
+++ routes/photos.js
@@ -3,10 +3,12 @@ var router = express.Router();
 require('./fp-api.js')()
 
 router.get('/', function(req, res, next) {
-  getPhotos({page: req.query.page, perpage: req.query.perpage})
+  var perpage = req.query.perpage || 100
+  var page = req.query.page || 1
+  getPhotos({page: page, perpage: perpage})
   .then(function(results) {
     results = results.map(function (person) { if (person.portrait) person.profile_photo = '/api/crop'+person.portrait.path; return person; })
-    res.render('photos', { people : results, nextpage: req.query.page+1, hasnextpage: results.length == req.query.perpage });
+    res.render('photos', { people : results, nextpage: page+1, hasnextpage: results.length == perpage });
   })
   .catch(function(err) {
     next(err)
